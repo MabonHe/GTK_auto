6490899c8914809e3a916f0d39c66d747408dec2
mabonhe <hemaobinz@163.com>
2018-08-28 00:48:53 +0800

    add analyzer file

---
+++ automation_tool/Analyzer/CMakeFiles/analyzer_app.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/analyzer_app.dir/analyzer.o"
+  "analyzer_app.pdb"
+  "analyzer_app"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/analyzer_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
